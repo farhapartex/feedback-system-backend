e06b5623d28af9f9d246331d2f88425e9364661b
Md Nazmul Hasan <farhapartex@gmail.com>
2023-02-26 01:18:02 +0600

    Added db connection and middleware

--- routes/feedbacks.js
+++ routes/feedbacks.js
@@ -6,4 +6,20 @@ router.get('/', (req, res) => {
     res.send('Hello World');
 })
 
+router.get('/:id', (req, res) => {
+    res.send('Feedback details');
+})
+
+router.post('/', (req, res) => {
+    res.send('New Feedback');
+})
+
+router.delete('/:id', (req, res) => {
+    res.send('Delete Feedback');
+})
+
+router.patch('/:id', (req, res) => {
+    res.send('Update Feedback');
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,30 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 const feedbacksRoutes = require('./routes/feedbacks');
 
 const app = express();
 
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
 // routes
 app.use('/api/feedbacks', feedbacksRoutes)
 
-app.listen(8000, () => {
-    console.log('Server is running on port 8000');
-});
 
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(process.env.PORT, () => {
+        console.log('Server is running on port 8000');
+    });
+}).catch((err) => {
+    console.log(err);
+})
