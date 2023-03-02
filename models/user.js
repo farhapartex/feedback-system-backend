2c3d185c53c935412f2f7a0c70ffdde81a2fda4d
Md Nazmul Hasan <farhapartex@gmail.com>
2023-03-03 01:23:37 +0600

    Added user schema

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: false,
+    },
+    lastName: {
+        type: String,
+        required: false,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+})
+
+
+module.exports = {
+    User: mongoose.model('User', userSchema)
+}
